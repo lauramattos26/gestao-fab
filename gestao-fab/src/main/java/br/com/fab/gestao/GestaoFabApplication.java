b2295fa4640c36e4c067e1e6ae81de8984074cd7
Laura Matos <lauramatosmartins17@gmail.com>
2025-11-06 21:27:57 -0300

    FEAT: Setup inicial do projeto Spring Boot com dependências

---
+++ gestao-fab/src/main/java/br/com/fab/gestao/GestaoFabApplication.java
@@ -0,0 +1,13 @@
+package br.com.fab.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFabApplication.class, args);
+	}
+
+}
---
+++ gestao-fab/src/test/java/br/com/fab/gestao/GestaoFabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fab.gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
