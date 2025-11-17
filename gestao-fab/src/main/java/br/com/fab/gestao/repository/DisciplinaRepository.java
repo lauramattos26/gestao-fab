fe6765a6b79394f439fc9a5998c655343fff39b0
Laura Matos <lauramatosmartins17@gmail.com>
2025-11-17 16:53:29 -0300

    FEAT: Adiciona camada de Serviço completa e corrige o prefixo do pacote para br.com.fab.gestao

--- gestao-fab/src/main/java/br/com/fab/gestao/repository/DisciplinaRepository.java
+++ gestao-fab/src/main/java/br/com/fab/gestao/repository/DisciplinaRepository.java
@@ -1,12 +1,10 @@
 package br.com.fab.gestao.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.fab.model.Disciplina;// Importa a Disciplina do pacote model
+import br.com.fab.model.Disciplina; // Importa a Disciplina do pacote model
 import java.util.List;
 
 public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
 
-    // Método para buscar disciplinas por um professor específico
-    // Isso será usado para o painel do professor
     List<Disciplina> findByProfessorId(Long professorId);
 }
